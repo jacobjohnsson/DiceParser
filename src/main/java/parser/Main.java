3e774b77fe9fdef180aad1189199a3817523fe9f
Jacob Johnsson <jacob.johnsson1992@gmail.com>
2020-05-10 10:36:43 +0200

    restructure of argument handling for n arguments

--- src/main/java/parser/Main.java
+++ src/main/java/parser/Main.java
@@ -8,25 +8,34 @@ import tokenizer.Token;
 
 public class Main {
   public static void main(String[] args) {
-    Main main = new Main();
-    for (String input : args) {
-      main.run(args);
-    }
+    new Main().run(args);
   }
 
   public void run(String[] args) {
-    String input = args[0];
 
+    for (int i = 0; i < args.length; i++) {
+      String input = args[i];
+      int result = parse(input);
+      System.out.println(input + ": " + result);
+    }
+
+
+  }
+
+  private int parse(String input) {
+    int result = 0;
     TokenIterator ti = new TokenIterator(input);
     Parser parser = new Parser(ti);
+
     try {
       Expression expr = parser.parse();
-      System.out.print(expr.eval());
+      result = expr.eval();
     } catch(Exception e) {
       System.out.println("Parsing went wrong.");
       System.out.println(e.getMessage());
       e.printStackTrace();
     }
 
+    return result;
   }
 }
